4c0b4da5a41994bf39d5194f73d992942fb78f34
yeray <yerayrome@outlook.com>
2017-01-10 20:31:48 +0100

    first commit

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '10.0'
+
+target ‘GeoJSON’ do
+  use_frameworks!
+
+  pod 'Alamofire', '~> 4.0'
+  pod 'SwiftyJSON'
+end
---
+++ Pods/Target Support Files/Pods-GeoJSON/Pods-GeoJSON-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GeoJSON : NSObject
+@end
+@implementation PodsDummy_Pods_GeoJSON
+@end
